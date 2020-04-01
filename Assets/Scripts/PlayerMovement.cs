f7e42e4a7818dbb8d06e60f543048a1b142966bd
Braian996 <braian.ledesma-99@hotmail.com>
2020-04-01 07:40:26 -0300

    Change the system movement and add a system rotation to the player
    Add extensions method to vectors and layer mask

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,7 @@
 ﻿using Enderlook.Unity.Atoms;
 
+using AvalonStudios.Extensions;
+
 using UnityEngine;
 
 namespace Game.Creatures.Player
@@ -7,18 +9,53 @@ namespace Game.Creatures.Player
     [RequireComponent(typeof(Rigidbody))]
     public class PlayerMovement : MonoBehaviour
     {
-        [SerializeField]
+        [SerializeField, Tooltip("Movement speed.")]
         private FloatGetReference speed;
 
         // We hide a Unity obsolete API
         private new Rigidbody rigidbody;
 
+        private Vector3 velocity;
+        private LayerMask layerGround = LayerMask.GetMask("Ground");
+
+        private int LayerGround => 1 << layerGround.ToLayer();
+
         private void Awake() => rigidbody = GetComponent<Rigidbody>();
 
-        private void Update()
+        private void FixedUpdate()
         {
-            float s = speed;
-            rigidbody.velocity = new Vector3(Input.GetAxis("Horizontal") * s, 0, Input.GetAxis("Vertical") * s);
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+
+            Move(h, v, speed);
+
+            Turn();
+        }
+
+        private void Move(float h, float v, float s)
+        {
+            velocity.Set(h, 0, v);
+
+            velocity = velocity.normalized * s * Time.deltaTime;
+
+            rigidbody.MovePosition(rigidbody.position + velocity);
+        }
+
+        private void Turn()
+        {
+            Ray camRayPoint = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit ground;
+
+            Vector3 pointOnScreen = Physics.Raycast(camRayPoint, out ground, LayerGround) ? ground.point : Vector3.zero;
+
+            Vector3 playerDirection = rigidbody.position.VectorSubtraction(pointOnScreen);
+
+            playerDirection.y = 0;
+
+            Quaternion newPlayerDirection = Quaternion.LookRotation(playerDirection);
+
+            rigidbody.MoveRotation(newPlayerDirection);
         }
     }
 }
