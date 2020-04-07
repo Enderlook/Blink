8b1f07e0b9ffc8fcc0105ad6e6702a47794b3a1b
Enderlook <enderlook@gmail.com>
2020-04-07 14:51:52 -0300

    Add EnemySpawner.GetSpawnPoint(Camera) method

--- Assets/Scripts/Scene/EnemySpawner.cs
+++ Assets/Scripts/Scene/EnemySpawner.cs
@@ -1,4 +1,6 @@
-﻿using Enderlook.Unity.Attributes;
+﻿using Enderlook.Extensions;
+
+using System;
 
 using UnityEngine;
 
@@ -6,10 +8,45 @@ namespace Game.Scene
 {
     public class EnemySpawner : MonoBehaviour
     {
-        [SerializeField, Tooltip("Points where enemies can spawn"), DrawVectorRelativeToTransform]
+        [SerializeField, Tooltip("Points where enemies can spawn")]
         private Vector3[] spawnPoints; // Keep name in sync with EnemySpawnerEditor
 
         [SerializeField, Tooltip("Determine the radius checked for collision on each point.")]
         private float distanceCheck = 1;
+
+        private const float ADDITIONAL_CHECK = 0.1f;
+
+        /// <summary>
+        /// Produce a random spawn point that is not being seen by <paramref name="camera"/>.
+        /// </summary>
+        /// <param name="camera"><see cref="Camera"/> to check if the point is visible to.</param>
+        /// <returns>A random point invisible to <paramref name="camera"/>.</returns>
+        public Vector3 GetSpawnPoint(Camera camera)
+        {
+            if (spawnPoints.Length == 0)
+                throw new InvalidOperationException($"{nameof(spawnPoints)} must not be empty.");
+
+            while (true)
+            {
+                Vector3 point = spawnPoints.RandomPick();
+
+                const float negativeCheck = 0 - ADDITIONAL_CHECK;
+                const float positiveCheck = 1 + ADDITIONAL_CHECK;
+
+                Vector3 viewPosition = camera.WorldToViewportPoint(point);
+                if (viewPosition.x < negativeCheck || viewPosition.x > positiveCheck
+                    || viewPosition.y < negativeCheck || viewPosition.y > positiveCheck
+                    || viewPosition.z <= 0)
+                {
+                    // Additional check to be sure
+                    Vector3 point2 = new Vector3(point.x, 0, point.z);
+                    viewPosition = camera.WorldToViewportPoint(point2);
+                    if (viewPosition.x < negativeCheck || viewPosition.x > positiveCheck
+                        || viewPosition.y < negativeCheck || viewPosition.y > positiveCheck
+                        || viewPosition.z <= 0)
+                        return point;
+                }
+            }
+        }
     }
 }
