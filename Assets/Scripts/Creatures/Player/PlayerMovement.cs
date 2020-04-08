2461e0ddbb89dddffea01e67a8029a54ec5e2393
Enderlook <enderlook@gmail.com>
2020-04-08 14:25:58 -0300

    Clean PlayerMovement code and supress IDE0051 warnings of Awake and FixedUpdate methods

--- Assets/Scripts/Creatures/Player/PlayerMovement.cs
+++ Assets/Scripts/Creatures/Player/PlayerMovement.cs
@@ -24,13 +24,14 @@ namespace Game.Creatures.Player
         // We hide a Unity obsolete API
         private new Rigidbody rigidbody;
 
-        private Vector3 velocity;
-        
+        private Vector3 velocity;        
 
         private int LayerGround => 1 << layerGround.ToLayer();
 
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
         private void Awake() => rigidbody = GetComponent<Rigidbody>();
 
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
         private void FixedUpdate()
         {
             float hor = Input.GetAxis("Horizontal");
@@ -58,9 +59,7 @@ namespace Game.Creatures.Player
         {
             Ray camRayPoint = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            RaycastHit ground;
-
-            Vector3 pointOnScreen = Physics.Raycast(camRayPoint, out ground, LayerGround) ? ground.point : Vector3.zero;
+            Vector3 pointOnScreen = Physics.Raycast(camRayPoint, out RaycastHit ground, LayerGround) ? ground.point : Vector3.zero;
 
             Vector3 playerDirection = rigidbody.position.VectorSubtraction(pointOnScreen);
 
