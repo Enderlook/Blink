fea83f9940a84619c88b2515398917c817dee4d2
Enderlook <enderlook@gmail.com>
2020-04-08 14:21:41 -0300

    Move MouseShooter into Game.Creatures.Player namespace and suppress warning IDE0051

--- Assets/Scripts/Creatures/Player/MouseShooter.cs
+++ Assets/Scripts/Creatures/Player/MouseShooter.cs
@@ -1,30 +1,35 @@
 ﻿using UnityEngine;
 
-public class MouseShooter : MonoBehaviour
+namespace Game.Creatures.Player
 {
-    public enum MouseButton { None = -1, Left = 0, Right = 1, Middle = 2 }
-
-    [SerializeField, Tooltip("Mouse button to shot.")]
-    private MouseButton mouseButton;
-
-    [SerializeField, Tooltip("Fire rate.")]
-    private float timeBtwAttack;
-
-    [SerializeField, Tooltip("Animator component")]
-    private Animator animator;
-
-    [SerializeField, Tooltip("Basic attack animation")]
-    private string basicAttack;
-
-    private float nextAttack;
-
-    void Update()
+    public class MouseShooter : MonoBehaviour
     {
-        if ((Input.GetMouseButtonDown((int)mouseButton) && mouseButton != MouseButton.None) && 
-            Time.time >= nextAttack)
+        public enum MouseButton { None = -1, Left = 0, Right = 1, Middle = 2 }
+
+        [SerializeField, Tooltip("Mouse button to shot.")]
+        private MouseButton mouseButton;
+
+        [SerializeField, Tooltip("Fire rate.")]
+        private float timeBtwAttack;
+
+        [SerializeField, Tooltip("Animator component")]
+        private Animator animator;
+
+        [SerializeField, Tooltip("Basic attack animation")]
+        private string basicAttack;
+
+        private float nextAttack;
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
+        private void Update()
         {
-            animator.SetTrigger(basicAttack);
-            nextAttack = Time.time + timeBtwAttack;
+            if (Input.GetMouseButtonDown((int)mouseButton)
+                && mouseButton != MouseButton.None
+                && Time.time >= nextAttack)
+            {
+                animator.SetTrigger(basicAttack);
+                nextAttack = Time.time + timeBtwAttack;
+            }
         }
     }
 }
