7c6b08784a1da8209edb0ee0f3aadee077ea8dae
Braian996 <braian.ledesma-99@hotmail.com>
2020-04-15 17:17:47 -0300

    Add VFX Hit

--- Assets/VFX AS/Scripts/ProjectileMotion.cs
+++ Assets/VFX AS/Scripts/ProjectileMotion.cs
@@ -11,8 +11,18 @@ public class ProjectileMotion : MonoBehaviour
     [SerializeField, Tooltip("Speed")]
     private float speed;
 
-    [SerializeField, Tooltip("Shoteable layer.")]
-    private LayerMask shoteableLayer;
+    [SerializeField, Tooltip("Enemy layer.")]
+    private LayerMask enemyLayer;
+
+    [Header("Prefabs")]
+
+    [SerializeField, Tooltip("Hit prefab.")]
+    private GameObject hitVFX;
+
+    [Header("Destroy Trails")]
+
+    [SerializeField, Tooltip("List of GameObjects attach to parent object.")]
+    private List<GameObject> trails;
 
     private void Start()
     {
@@ -24,9 +34,50 @@ public class ProjectileMotion : MonoBehaviour
             rb.velocity = (rb.transform.forward * speed);
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision collider)
     {
-        if (other.gameObject.layer != 5)
-            Destroy(gameObject);
+        if (collider.gameObject.layer != 5)
+        {
+            ContactPoint contact = collider.contacts[0];
+            Quaternion contactRotation = Quaternion.FromToRotation(Vector3.up, contact.normal);
+            Vector3 contactPosition = contact.point;
+
+            if (hitVFX != null)
+            {
+                GameObject hitObj = Instantiate(hitVFX, contactPosition, contactRotation);
+
+                ParticleSystem psHit = hitObj.GetComponent<ParticleSystem>();
+
+                ParticleSystem ps = psHit == null ? hitObj.transform.GetChild(0).GetComponent<ParticleSystem>() : psHit;
+
+                Destroy(hitObj, ps.main.duration);
+            }
+
+            StartCoroutine(DestroyParticles(0));
+            //Destroy(gameObject);
+        }
+    }
+
+    public IEnumerator DestroyParticles (float waitTime)
+    {
+        if (transform.childCount > 0 && waitTime != 0)
+        {
+            List<Transform> transforms = new List<Transform>();
+
+            foreach (Transform t in transform.GetChild(0).transform)
+                transforms.Add(t);
+
+            while (transform.GetChild(0).localScale.x > 0)
+            {
+                yield return new WaitForSeconds(0.01f);
+                Vector3 newScale = new Vector3(.1f, .1f, .1f);
+                transform.GetChild(0).localScale -= newScale;
+                foreach (Transform t in transforms)
+                    t.localScale -= newScale;
+            }
+        }
+
+        yield return new WaitForSeconds(waitTime);
+        Destroy(gameObject);
     }
 }
