06393ab285fe3d37b5c2549fe1eb706fbe86a859
Enderlook <enderlook@gmail.com>
2020-04-22 11:41:01 -0300

    Add menu items to EnemyData and EnemyLevelData

--- Assets/Scripts/Scene/Spawn System/EnemyData.cs
+++ Assets/Scripts/Scene/Spawn System/EnemyData.cs
@@ -1,7 +1,11 @@
 ﻿using Enderlook.Unity.Prefabs.FloatingText;
-
+#if UNITY_EDITOR
+using Enderlook.Unity.Utils.UnityEditor;
+#endif
 using Game.Creatures;
-
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 using UnityEngine;
 
 namespace Game.Scene
@@ -47,5 +51,25 @@ namespace Game.Scene
 
         private float GetValue(Vector4 parameters)
             => Mathf.Min((GameManager.Difficulty - parameters.x) * parameters.y, parameters.z) + parameters.w;
+
+#if UNITY_EDITOR
+        [MenuItem("Assets/Game/Create Enemy Data")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
+        private static void CreateEnemyData()
+        {
+            foreach (GameObject enemy in Selection.GetFiltered<GameObject>(SelectionMode.TopLevel))
+            {
+                EnemyData enemyData = CreateInstance<EnemyData>();
+                enemyData.enemyPrefab = enemy;
+                enemyData.name = enemy.name;
+                AssetDatabase.CreateAsset(enemyData, AssetDatabase.GetAssetPath(enemy).WithDifferentExtension("asset"));
+            }
+        }
+
+        [MenuItem("Assets/Game/Create Enemy Data", true)]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
+        private static bool CreateEnemyDataValidator()
+            => Selection.GetFiltered<GameObject>(SelectionMode.TopLevel).Length > 0;
+#endif
     }
 }
