2fea73553514027afce6632c6293aa6d4fa69922
Enderlook <enderlook@gmail.com>
2020-04-26 13:37:31 -0300

    Add sound to projectiles and hit

---
+++ Assets/Scripts/Attacks/RandomPitch.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Game.Attacks
+{
+    [AddComponentMenu("Game/Attacks/Random Pitch")]
+    [RequireComponent(typeof(AudioSource))]
+    public class RandomPitch : MonoBehaviour
+    {
+        [SerializeField, Tooltip("Random pitch range.")]
+        private Vector2 range = new Vector2(.8f, 1.2f);
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
+        private void Awake() => GetComponent<AudioSource>().pitch = Random.Range(range.x, range.y);
+    }
+}
