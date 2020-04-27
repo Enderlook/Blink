b2cccf917cfea52942077693cc39fbc6e91fea52
Braian996 <braian.ledesma-99@hotmail.com>
2020-04-27 17:59:46 -0300

    Enable attack animations

---
+++ Assets/Scripts/Creatures/Enemies/EnemyAttack.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Creatures
+{
+    public class EnemyAttack : MonoBehaviour
+    {
+        [SerializeField, Tooltip("Enemy path finding.")]
+        private EnemyPathFinding enemyPathFinding;
+
+        [SerializeField, Tooltip("Animator component.")]
+        private Animator animator;
+
+        [SerializeField, Tooltip("Animation key")]
+        private string animationKey;
+
+        [SerializeField, Tooltip("Cooldown")]
+        private float cooldown;
+
+        private float nextAttack;
+
+        private void Update()
+        {
+            if (enemyPathFinding.TargetDistance <= enemyPathFinding.ThisNavMeshAgent.stoppingDistance)
+            {
+                if (Time.time >= nextAttack)
+                {
+                    animator.SetTrigger(animationKey);
+                    nextAttack = Time.time + cooldown;
+                }
+            }
+        }
+
+    }
+}
