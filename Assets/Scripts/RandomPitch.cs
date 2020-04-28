3e752d614c1cb02d894c52cd2ae7d59a3c815693
Enderlook <enderlook@gmail.com>
2020-04-27 23:49:36 -0300

    Move RandomPitch from Scripts/Attacks to Scripts

--- Assets/Scripts/RandomPitch.cs
+++ Assets/Scripts/RandomPitch.cs
@@ -9,7 +9,15 @@ namespace Game.Attacks
         [SerializeField, Tooltip("Random pitch range.")]
         private Vector2 range = new Vector2(.8f, 1.2f);
 
+        private AudioSource audioSource;
+
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
-        private void Awake() => GetComponent<AudioSource>().pitch = Random.Range(range.x, range.y);
+        private void Awake()
+        {
+            audioSource = GetComponent<AudioSource>();
+            Randomize();
+        }
+
+        public void Randomize() => audioSource.pitch = Random.Range(range.x, range.y);
     }
 }
