4e7a9fcb83aa4e6c0886bd0356000e08da7b3544
Enderlook <enderlook@gmail.com>
2020-04-27 23:52:03 -0300

    Add hurt and die sounds to player and cristal

--- Assets/Scripts/Creatures/Hurtable.cs
+++ Assets/Scripts/Creatures/Hurtable.cs
@@ -1,9 +1,12 @@
-﻿using Enderlook.Unity.Atoms;
-
+﻿using Enderlook.Extensions;
+using Enderlook.Unity.Atoms;
+using Enderlook.Unity.Components.Destroy;
+using Game.Attacks;
 using UnityEngine;
 
 namespace Game.Creatures
 {
+    [RequireComponent(typeof(AudioSource)), RequireComponent(typeof(RandomPitch))]
     public class Hurtable : MonoBehaviour, IDamagable
     {
 #pragma warning disable CS0649
@@ -12,13 +15,46 @@ namespace Game.Creatures
 
         [SerializeField, Tooltip("Maximum health.")]
         private IntGetSetReference maxHealth;
+
+        [SerializeField, Tooltip("Sounds play when hurt.")]
+        private AudioClip[] hurtSounds;
+
+        [SerializeField, Tooltip("Sounds play when die.")]
+        private AudioClip[] dieSounds;
 #pragma warning restore CS0649
 
         public int Health => health;
 
         public int MaxHealth => maxHealth;
 
-        public void TakeDamage(int amount) => health.SetValue(health.GetValue() - amount);
+        private AudioSource audioSource;
+
+        private RandomPitch randomPitch;
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
+        private void Awake()
+        {
+            audioSource = GetComponent<AudioSource>();
+            randomPitch = GetComponent<RandomPitch>();
+        }
+
+        public void TakeDamage(int amount)
+        {
+            randomPitch.Randomize();
+            health.SetValue(health.GetValue() - amount);
+            audioSource.clip = hurtSounds.RandomPick();
+            audioSource.Play();
+
+            if (health.GetValue() == 0)
+            {
+                GameObject go = new GameObject($"Die Sound of {gameObject.name}");
+                AudioSource source = go.AddComponent<AudioSource>();
+                source.clip = dieSounds.RandomPick();
+                go.AddComponent<RandomPitch>();
+                source.Play();
+                go.AddComponent<DestroyWhenAudioSourceEnds>();
+            }
+        }
 
         public void TakeHealing(int amount) => health.SetValue(health.GetValue() + amount);
 
