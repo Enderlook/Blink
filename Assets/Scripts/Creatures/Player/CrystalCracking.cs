dc84afea52dc68800ed5613887ad50f7ea48c961
Enderlook <enderlook@gmail.com>
2020-05-20 10:33:20 -0300

    Fix CrystalCracking

--- Assets/Scripts/Creatures/Player/CrystalCracking.cs
+++ Assets/Scripts/Creatures/Player/CrystalCracking.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace Game.Creatures
 {
-    [AddComponentMenu("Game/Creatures/Crystal/Cracking")]
+    [AddComponentMenu("Game/Creatures/Crystal/Cracking"), RequireComponent(typeof(MeshRenderer))]
     public class CrystalCracking : MonoBehaviour
     {
 #pragma warning disable CS0649
@@ -16,18 +16,20 @@ namespace Game.Creatures
 
         [SerializeField]
         private IntEventReference healthEvent;
-
-        [SerializeField]
-        private Material materialCrystal;
 #pragma warning restore CS0649
+
+        private Material material;
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
         private void Awake()
         {
             healthEvent.Register(OnChange);
-            materialCrystal.SetFloat("_Health", (float)health / maxHealth);
+            MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+            material = new Material(meshRenderer.material);
+            meshRenderer.material = material;
+            material.SetFloat("_Health", (float)health / maxHealth);
         }
 
-        public void OnChange(int _) => materialCrystal.SetFloat("_Health", (float)health / maxHealth);
+        public void OnChange(int _) => material.SetFloat("_Health", (float)health / maxHealth);
     }
 }
