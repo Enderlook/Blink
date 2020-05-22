94706b23d7a41f4d66318520cb3599c9531a8d3d
Enderlook <enderlook@gmail.com>
2020-05-22 08:57:24 -0300

    Reorganice files

--- Assets/Scripts/Others/PlayWithDelay.cs
+++ Assets/Scripts/Others/PlayWithDelay.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-namespace Game.Attacks
+namespace Game
 {
     [RequireComponent(typeof(AudioSource)), AddComponentMenu("Game/Others/Play With Delay")]
     public class PlayWithDelay : MonoBehaviour
