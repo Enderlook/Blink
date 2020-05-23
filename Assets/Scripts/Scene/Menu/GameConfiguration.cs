dcd715b01d7688fd8a53b2a74e9aa700521eb2ba
Braian996 <braian.ledesma-99@hotmail.com>
2020-05-23 04:30:23 -0300

    Add GameConfiguration script

---
+++ Assets/Scripts/Scene/Menu/GameConfiguration.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class GameConfiguration : MonoBehaviour
+{
+    [Header("UI Components")]
+
+    [SerializeField, Tooltip("Component dropdown of resolution.")]
+    private Dropdown resolutionDropdown = null;
+
+    private List<Resolution> resolutions = new List<Resolution>();
+
+    private static int currentResolutionIndex;
+
+    public void Start()
+    {
+        SetResolutionsInDropdown();
+    }
+
+    private void SetResolutionsInDropdown()
+    {
+
+        resolutions = Screen.resolutions.ToList();
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> optionsInDropdown = new List<string>();
+
+        foreach (Resolution resolution in resolutions)
+        {
+            string option = $"{resolution.width}x{resolution.height}";
+            optionsInDropdown.Add(option);
+
+            currentResolutionIndex = resolution.width == Screen.currentResolution.width
+                && resolution.height == Screen.currentResolution.height ? resolutions.IndexOf(resolution) : currentResolutionIndex;
+        }
+
+        resolutionDropdown.AddOptions(optionsInDropdown);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int index)
+    {
+        currentResolutionIndex = index;
+        Screen.SetResolution(resolutions[index].width, resolutions[index].height, Screen.fullScreen);
+    }
+}
