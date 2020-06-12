3783e7b198e16cfbd7f24abb79598a7aaf675360
Enderlook <enderlook@gmail.com>
2020-06-12 09:59:39 -0300

    Fix AbilityUI

--- Assets/Scripts/Creatures/Player/Ability System/UI/AbilityUI.cs
+++ Assets/Scripts/Creatures/Player/Ability System/UI/AbilityUI.cs
@@ -50,8 +50,8 @@ namespace Game.Creatures.Player.AbilitySystem
             }
 #endif
 #else
-            _toggle.targetGraphic.color = _toggle.colors.normalColor;
-            _toggle.enabled = false;
+            Toggle.targetGraphic.color = _toggle.colors.normalColor;
+            Toggle.enabled = false;
 #endif
         }
 
