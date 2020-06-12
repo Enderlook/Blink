61a27baa762857db1db0caba902c743779f66cbf
Enderlook <enderlook@gmail.com>
2020-06-12 09:58:49 -0300

    Fix DestroyIfNotAndroid

--- Assets/Scripts/Scene/Menu/DestroyIfNotAndroid.cs
+++ Assets/Scripts/Scene/Menu/DestroyIfNotAndroid.cs
@@ -12,7 +12,7 @@ namespace Game.Scene
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
         private void Awake()
         {
-#if UNITY_EDITOR
+#if UNITY_EDITOR && UNITY_ANDROID
             if (!UnityEditor.EditorApplication.isRemoteConnected && removeIfRemoteIsNotConnected)
 #endif
                 Destroy(gameObject);
