3c0bb09eaa5cb6dbb1a2887dcf47eafc60427cd5
Braian996 <braian.ledesma-99@hotmail.com>
2020-06-15 22:40:13 -0300

    Set up new AoE ability of Crystal

--- Assets/Scripts/Creatures/Player/Ability System/Abilities/Components/SelfAreaOfDamageAbilityComponent.cs
+++ Assets/Scripts/Creatures/Player/Ability System/Abilities/Components/SelfAreaOfDamageAbilityComponent.cs
@@ -1,15 +1,25 @@
-﻿using UnityEngine;
+﻿using Game.Scene;
+
+using UnityEngine;
 
 namespace Game.Creatures.Player.AbilitySystem
 {
     [CreateAssetMenu(fileName = "Self Area Of Damage Ability", menuName = "Game/Ability System/Components/Self Area Of Damage")]
     public class SelfAreaOfDamageAbilityComponent : AreaOfDamageAbilityComponent
     {
-        private Transform player;
+        [SerializeField]
+        private bool useCrystal = false;
 
-        public override void Initialize(AbilitiesManager manager)
-            => player = manager.transform;
+        private Transform target;
 
-        public override void Execute() => InstantiatePrefab(player.position, player.rotation);
+        public override void Initialize(AbilitiesManager manager) => target = useCrystal ? CrystalAndPlayerTracker.Crystal : manager.transform;
+
+        public override void Execute()
+        {
+            if (useCrystal) 
+                InstantiatePrefab(target.position); 
+            else 
+                InstantiatePrefab(target.position, target.rotation);
+        }
     }
 }
