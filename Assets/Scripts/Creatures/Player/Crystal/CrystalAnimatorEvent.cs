6ddcc08ca192f04a9072d5792e749d06b467e599
Enderlook <enderlook@gmail.com>
2020-06-16 18:41:04 -0300

    Fix pragma warnings

--- Assets/Scripts/Creatures/Player/Crystal/CrystalAnimatorEvent.cs
+++ Assets/Scripts/Creatures/Player/Crystal/CrystalAnimatorEvent.cs
@@ -6,14 +6,15 @@ namespace Game.Creatures.Crystal
 {
     public class CrystalAnimatorEvent : MonoBehaviour
     {
+#pragma warning disable CS0649
         [SerializeField]
         private Animator animator;
+#pragma warning restore CS0649
 
         public void Teleportation()
         {
             animator.SetBool("Teleportation", false);
             Menu.Instance.Win();
-        }
-    
+        }    
     }
 }
--- Assets/Scripts/Scene/Menu/DestroyIfNotAndroid.cs
+++ Assets/Scripts/Scene/Menu/DestroyIfNotAndroid.cs
@@ -6,8 +6,10 @@ namespace Game.Scene
     public class DestroyIfNotAndroid : MonoBehaviour
     {
 #if !UNITY_ANDROID || UNITY_EDITOR
+#pragma warning disable CS0649, CS0414
         [SerializeField]
         private bool removeIfRemoteIsNotConnected = true;
+#pragma warning restore CS0649, CS0414
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
         private void Awake()
