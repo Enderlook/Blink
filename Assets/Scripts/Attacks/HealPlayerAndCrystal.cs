4a4775a8031bca06c346da80acf7fc57c1d5072f
Enderlook <enderlook@gmail.com>
2020-06-21 12:17:16 -0300

    Fix Healing Aura ability

---
+++ Assets/Scripts/Attacks/HealPlayerAndCrystal.cs
@@ -0,0 +1,28 @@
+﻿using Game.Creatures;
+using Game.Scene;
+
+using System.Collections;
+
+using UnityEngine;
+
+namespace Game.Attacks
+{
+    [RequireComponent(typeof(Collider)), AddComponentMenu("Game/Attacks/Heal Player And Crystal")]
+    public class HealPlayerAndCrystal : MonoBehaviour
+    {
+        public static void AddComponentTo(GameObject source, float timeToHeal, int playerHealing, int crystalHealing)
+        {
+            HealPlayerAndCrystal component = source.AddComponent<HealPlayerAndCrystal>();
+
+            component.StartCoroutine(Work());
+
+            IEnumerator Work()
+            {
+                yield return new WaitForSeconds(timeToHeal);
+                CrystalAndPlayerTracker.CrystalHurtable.TakeHealing(crystalHealing);
+                CrystalAndPlayerTracker.Player.GetComponent<Hurtable>().TakeHealing(playerHealing);
+                Destroy(component);
+            }
+        }
+    }
+}
--- Assets/Scripts/Creatures/Player/Ability System/Abilities/Components/HealPlayerAndCrystalAbilityComponent.cs
+++ Assets/Scripts/Creatures/Player/Ability System/Abilities/Components/HealPlayerAndCrystalAbilityComponent.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 
 namespace Game.Creatures.Player.AbilitySystem
 {
-    [CreateAssetMenu(fileName = "Area Of Healing Ability", menuName = "Game/Ability System/Components/Area Of Healing")]
-    public class AreaOfHealingAbilityComponent : AbilityComponent
+    [CreateAssetMenu(fileName = "Heal Player And Crystal Ability", menuName = "Game/Ability System/Components/Heal Player And Crystal")]
+    public class HealPlayerAndCrystalAbilityComponent : AbilityComponent
     {
 #pragma warning disable CS0649
         [Header("Configuration")]
-        [SerializeField, Tooltip("Health restored by the healing aura")]
-        protected int healing = 25;
+        [SerializeField, Tooltip("Health restored to player.")]
+        protected int playerHealing = 25;
+
+        [SerializeField, Tooltip("Health restored to crystal.")]
+        protected int crystalHealing = 25;
 
         [SerializeField, Tooltip("Time in seconds since the prefab is spawn to produce the healing.")]
         private float timeToProduceHealing = 1;
-
-        [SerializeField, Tooltip("Layer to affect.")]
-        private LayerMask hitLayer;
 
         [Header("Setup")]
         [SerializeField, Tooltip("Spawned prefab.")]
@@ -32,7 +32,7 @@ namespace Game.Creatures.Player.AbilitySystem
         public override void Execute()
         {
             GameObject instance = Instantiate(prefab, spawnPoint.position, Quaternion.identity);
-            AreaOfHealing.AddComponentTo(instance, timeToProduceHealing, healing, hitLayer);
+            HealPlayerAndCrystal.AddComponentTo(instance, timeToProduceHealing, playerHealing, crystalHealing);
         }
     }
 }
