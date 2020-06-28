cef70f21f58573a1affadb5683ddbab040fae048
Enderlook <enderlook@gmail.com>
2020-06-28 18:57:42 -0300

    Add DestroyIfAndroid

---
+++ Assets/Scripts/Scene/Menu/DestroyIfAndroid.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Game.Scene
+{
+    [AddComponentMenu("Game/Destroy If Android")]
+    public class DestroyIfAndroid : MonoBehaviour
+    {
+#if UNITY_EDITOR
+#pragma warning disable CS0649, CS0414
+        [SerializeField]
+        private bool notRemoveIfRemoteIsConnected;
+#pragma warning restore CS0649, CS0414
+#endif
+
+#if UNITY_ANDROID
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
+        private void Awake()
+        {
+#if UNITY_EDITOR
+            if (!(notRemoveIfRemoteIsConnected && UnityEditor.EditorApplication.isRemoteConnected))
+#endif
+                Destroy(gameObject);
+        }
+#endif
+    }
+}
