4a0d093006e4a89b62abd161b9ac384cc778c5f4
Enderlook <enderlook@gmail.com>
2020-06-28 19:08:16 -0300

    Add InstantiateIfNotAndroid

--- Assets/Scripts/Scene/Menu/DestroyIfNotAndroid.cs
+++ Assets/Scripts/Scene/Menu/DestroyIfNotAndroid.cs
@@ -5,12 +5,13 @@ namespace Game.Scene
     [AddComponentMenu("Game/Destroy If Not Android")]
     public class DestroyIfNotAndroid : MonoBehaviour
     {
-#if !UNITY_ANDROID || UNITY_EDITOR
+#if UNITY_ANDROID || UNITY_EDITOR
+#if UNITY_EDITOR
 #pragma warning disable CS0649, CS0414
         [SerializeField]
         private bool removeIfRemoteIsNotConnected = true;
 #pragma warning restore CS0649, CS0414
-
+#endif
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
         private void Awake()
         {
---
+++ Assets/Scripts/Scene/Menu/InstantiateIfNotAndroid.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Game.Scene
+{
+    [AddComponentMenu("Game/Instantiate If Not Android")]
+    public class InstantiateIfNotAndroid : MonoBehaviour
+    {
+#pragma warning disable CS0649, CS0414
+#if !UNITY_ANDROID || UNITY_EDITOR
+#if UNITY_EDITOR
+        [SerializeField]
+        private bool addIfRemoteIsNotConnected = true;
+#endif
+
+        [SerializeField, Tooltip("Prefab to instantiate as child.")]
+        private GameObject prefab;
+#pragma warning restore CS0649, CS0414
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
+        private void Awake()
+        {
+#if !UNITY_ANDROID || UNITY_EDITOR
+#if UNITY_EDITOR && !IGNORE_UNITY_REMOTE
+            if (!UnityEditor.EditorApplication.isRemoteConnected && addIfRemoteIsNotConnected)
+#endif
+                Instantiate(prefab, transform);
+#endif
+        }
+#endif
+    }
+}
