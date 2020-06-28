b66a53361b16849b2140f5dc3b3628be2dc40b52
Enderlook <enderlook@gmail.com>
2020-06-28 19:21:36 -0300

    Add IGNORE_UNITY_REMOTE in DestroyIfAndroid

--- Assets/Scripts/Scene/Menu/DestroyIfAndroid.cs
+++ Assets/Scripts/Scene/Menu/DestroyIfAndroid.cs
@@ -16,7 +16,7 @@ namespace Game.Scene
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
         private void Awake()
         {
-#if UNITY_EDITOR
+#if UNITY_EDITOR && !IGNORE_UNITY_REMOTE
             if (!(notRemoveIfRemoteIsConnected && UnityEditor.EditorApplication.isRemoteConnected))
 #endif
                 Destroy(gameObject);
