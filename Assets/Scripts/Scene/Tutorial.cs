686d56db1c9db00d86872256029315c2c9d7350d
Enderlook <enderlook@gmail.com>
2020-07-15 14:57:29 -0300

    Fix Tutorial to only show once

--- Assets/Scripts/Scene/Tutorial.cs
+++ Assets/Scripts/Scene/Tutorial.cs
@@ -12,6 +12,16 @@ namespace Game.Scene
         private string[] parametersAnimation;
 #pragma warning restore CS0649
 
+        private static bool alreadyShowed;
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Used by Unity.")]
+        private void Awake()
+        {
+            if (alreadyShowed)
+                Destroy(gameObject);
+            alreadyShowed = true;
+        }
+
         public void NextAnimation() => animator.SetTrigger(parametersAnimation[0]);
 
         public void CloseAnimation() => animator.SetBool(parametersAnimation[1], true);
